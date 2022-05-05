ff2a4c3cbb927011d208d3c2dc50b38709ef2351
Meilyn22 <samsonidoko3@byui.edu>
2022-05-04 20:25:47 -0600

    Updates

---
+++ precache-manifest.045d1092423c3e28dee21fd665b83a3f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1e0289b1ec3b3420112",
+    "url": "/static/js/main.b1e0289b.chunk.js"
+  },
+  {
+    "revision": "a8952a02798b7b7005f2",
+    "url": "/static/js/1.a8952a02.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c9e664694021e0d478575505c729541b",
+    "url": "/index.html"
+  }
+];
